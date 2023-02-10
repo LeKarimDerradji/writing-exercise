be8eb65201b2654b1682778b43484cb0a7a9f8ae
LeKarimDerradji <karim.derradji.pro@gmail.com>
2023-02-10 21:45:32 +0100

    remmapings

--- test/Poc.t.sol
+++ test/Poc.t.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.10;
 
-import "forge-std/Test.sol";
+
 import "../src/Implementation.sol";
+import "forge-std/Test.sol";
 import "../src/Proxy.sol";
 
 contract SimplePocTest is Test {
